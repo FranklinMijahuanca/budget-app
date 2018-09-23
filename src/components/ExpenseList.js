747c37a8c8269054f00f4d24808f3944fcc5601a
zFranklin <franklin.mijahuanca@unas.edu.pe>
2018-09-23 17:24:14 -0500

    Projet init

---
+++ src/components/ExpenseList.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import ExpenseListItem from './ExpenseListItem';
+import selectExpenses from '../selectors/expenses';
+
+export const ExpenseList = (props) => (
+  <div className="content-container">
+    <div className="list-header">
+      <div className="show-for-mobile">Gastos</div>
+      <div className="show-for-desktop">Gasto</div>
+      <div className="show-for-desktop">Monto</div>
+    </div>
+    <div className="list-body">
+      {
+        props.expenses.length === 0 ? (
+          <div className="list-item list-item--message">
+            <span>Sin gastos</span>
+          </div>
+        ) : (
+            props.expenses.map((expense) => {
+              return <ExpenseListItem key={expense.id} {...expense} />;
+            })
+          )
+      }
+    </div>
+  </div>
+);
+
+const mapStateToProps = (state) => {
+  return {
+    expenses: selectExpenses(state.expenses, state.filters)
+  };
+};
+
+export default connect(mapStateToProps)(ExpenseList);
